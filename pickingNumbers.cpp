fa1c53d9f2da905713b5983d74c4a92f6947069e
Ogulcan Bozkurt <114852871+owdx1@users.noreply.github.com>
2023-06-27 19:37:27 +0300

    Create pickingNumbers.cpp

---
+++ pickingNumbers.cpp
@@ -0,0 +1,152 @@
+/*
+Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to .
+
+Example
+
+
+There are two subarrays meeting the criterion:  and . The maximum length subarray has  elements.
+
+Function Description
+
+Complete the pickingNumbers function in the editor below.
+
+pickingNumbers has the following parameter(s):
+
+int a[n]: an array of integers
+Returns
+
+int: the length of the longest subarray that meets the criterion
+Input Format
+
+The first line contains a single integer , the size of the array .
+The second line contains  space-separated integers, each an .
+
+Constraints
+
+The answer will be .
+Sample Input 0
+
+6
+4 6 5 3 3 1
+Sample Output 0
+
+3
+Explanation 0
+
+We choose the following multiset of integers from the array: . Each pair in the multiset has an absolute difference  (i.e.,  and ), so we print the number of chosen integers, , as our answer.
+
+Sample Input 1
+
+6
+1 2 2 3 1 2
+Sample Output 1
+
+5
+Explanation 1
+
+We choose the following multiset of integers from the array: . Each pair in the multiset has an absolute difference  (i.e., , , and ), so we print the number of chosen integers, , as our answer.
+
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+int pickingNumbers(vector<int> a) {
+    vector<int> counts(100, 0);
+
+    for (int i : a) {
+        counts[i]++;
+    }
+
+    int maxCount = 0;
+    for (int i = 0; i < counts.size() - 1; i++) {
+        maxCount = max(maxCount, counts[i] + counts[i + 1]);
+    }
+
+    return maxCount;
+}
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    int result = pickingNumbers(a);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+*/
