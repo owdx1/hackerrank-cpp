9ca41a7a25e62cdcbf7e02de4de4b6bff4d7d118
Ogulcan Bozkurt <114852871+owdx1@users.noreply.github.com>
2023-06-27 18:51:39 +0300

    Create minimumDistances.cpp

---
+++ minimumDistances.cpp
@@ -0,0 +1,156 @@
+/*
+The distance between two array values is the number of indices between them. Given , find the minimum distance between any pair of equal elements in the array. If no such value exists, return .
+
+Example
+
+There are two matching pairs of values:  and . The indices of the 's are  and , so their distance is . The indices of the 's are  and , so their distance is . The minimum distance is .
+
+Function Description
+
+Complete the minimumDistances function in the editor below.
+
+minimumDistances has the following parameter(s):
+
+int a[n]: an array of integers
+Returns
+
+int: the minimum distance found or  if there are no matching elements
+Input Format
+
+The first line contains an integer , the size of array .
+The second line contains  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print a single integer denoting the minimum  in . If no such value exists, print .
+
+Sample Input
+
+STDIN           Function
+-----           --------
+6               arr[] size n = 6
+7 1 3 4 1 7     arr = [7, 1, 3, 4, 1, 7]
+Sample Output
+
+3
+Explanation
+There are two pairs to consider:
+
+ and  are both , so .
+ and  are both , so .
+The answer is .
+*/
+
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'minimumDistances' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+ 
+
+int minimumDistances(vector<int> a) {
+    int len = a.size();
+    int minimumDistance = 2147483647;
+    bool flag1 = false;
+    
+    for(int i = 0; i < len; i++){
+        for(int j = i + 1; j < len;j++){
+            if(a[i] == a[j]){
+                if(!(minimumDistance < j - i)){
+                    minimumDistance = j - i;
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    if (minimumDistance == 2147483647 ) return -1;
+    
+    return minimumDistance;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    int result = minimumDistances(a);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
