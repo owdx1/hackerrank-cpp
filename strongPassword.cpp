8df5b929fb294c5593de06d7287659f1e1660512
Ogulcan Bozkurt <114852871+owdx1@users.noreply.github.com>
2023-06-29 18:51:56 +0300

    Create strongPassword.cpp

---
+++ strongPassword.cpp
@@ -0,0 +1,159 @@
+/*Louise joined a social networking site to stay in touch with her friends. The signup page required her to input a name and a password. However, the password must be strong. The website considers a password to be strong if it satisfies the following criteria:
+
+Its length is at least .
+It contains at least one digit.
+It contains at least one lowercase English character.
+It contains at least one uppercase English character.
+It contains at least one special character. The special characters are: !@#$%^&*()-+
+She typed a random string of length  in the password field but wasn't sure if it was strong. Given the string she typed, can you find the minimum number of characters she must add to make her password strong?
+
+Note: Here's the set of types of characters in a form you can paste in your solution:
+
+numbers = "0123456789"
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+special_characters = "!@#$%^&*()-+"
+Example
+
+
+This password is 5 characters long and is missing an uppercase and a special character. The minimum number of characters to add is .
+
+
+This password is 5 characters long and has at least one of each character type. The minimum number of characters to add is .
+
+Function Description
+
+Complete the minimumNumber function in the editor below.
+
+minimumNumber has the following parameters:
+
+int n: the length of the password
+string password: the password to test
+Returns
+
+int: the minimum number of characters to add
+Input Format
+
+The first line contains an integer , the length of the password.
+
+The second line contains the password string. Each character is either a lowercase/uppercase English alphabet, a digit, or a special character.
+
+Constraints
+
+All characters in  are in [a-z], [A-Z], [0-9], or [!@#$%^&*()-+ ].
+Sample Input 0
+
+3
+Ab1
+Sample Output 0
+
+3
+Explanation 0
+
+She can make the password strong by adding  characters, for example, $hk, turning the password into Ab1$hk which is strong.
+
+ characters aren't enough since the length must be at least .
+
+Sample Input 1
+
+11
+#HackerRank
+Sample Output 1
+
+1
+Explanation 1
+
+The password isn't strong, but she can make it strong by adding a single digit. */
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'minimumNumber' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. STRING password
+ */
+
+int minimumNumber(int n, string password) {
+    int conds[] = {0,0,0,0};
+    for(int i = 0; i < password.size(); i++) {
+        char c = password[i];
+        if(c >= '0' && c <= '9') {
+            conds[0]++;
+        }else if(c >= 'a' && c <= 'z') {
+            conds[1]++; 
+        }else if(c >= 'A' && c <= 'Z') {
+            conds[2]++;
+        }else{
+            string specialChars = "!@#$%^&*()-+";
+            for(int j = 0; j < specialChars.size() ; j++ ) {
+                if(c == specialChars[j]) {
+                    conds[3]++;
+                }
+            }
+        }
+    }
+
+    int missing = 0;
+    for(int i = 0; i < 4; i++) {
+        if(conds[i] == 0) {
+            missing++;
+        }
+    }
+
+    if((6-n) > missing) {
+        return (6-n);
+    }
+    return missing;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string password;
+    getline(cin, password);
+
+    int answer = minimumNumber(n, password);
+
+    fout << answer << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
