ea28fd4dc86e4f39f727d99d070f9ab8ba774095
Ogulcan Bozkurt <114852871+owdx1@users.noreply.github.com>
2023-07-01 14:18:53 +0300

    Create utopianTree.cpp

---
+++ utopianTree.cpp
@@ -0,0 +1,126 @@
+/* The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter.
+
+A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after  growth cycles?
+
+For example, if the number of growth cycles is , the calculations are as follows:
+
+Period  Height
+0          1
+1          2
+2          3
+3          6
+4          7
+5          14
+Function Description
+
+Complete the utopianTree function in the editor below.
+
+utopianTree has the following parameter(s):
+
+int n: the number of growth cycles to simulate
+Returns
+
+int: the height of the tree after the given number of cycles
+Input Format
+
+The first line contains an integer, , the number of test cases.
+ subsequent lines each contain an integer, , the number of cycles for that test case.
+
+Constraints
+
+
+
+Sample Input
+
+3
+0
+1
+4
+Sample Output
+
+1
+2
+7
+Explanation
+
+There are 3 test cases.
+
+In the first case (), the initial height () of the tree remains unchanged.
+
+In the second case (), the tree doubles in height and is  meters tall after the spring cycle.
+
+In the third case (), the tree doubles its height in spring (, ), then grows a meter in summer (, ), then doubles after the next spring (, ), and grows another meter after summer (, ). Thus, at the end of 4 cycles, its height is  meters.
+  */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'utopianTree' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+int utopianTree(int n) {
+    int i = 0;
+    int height = 0;
+    while ( i <= n ){
+        if(i % 2 == 0) {height += 1;}
+        else{height *= 2;}
+        i++;
+    }
+    
+    return height;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        int result = utopianTree(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
