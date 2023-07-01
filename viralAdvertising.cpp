dc20d9df9385b8df1623a3bddaf942018877746b
Ogulcan Bozkurt <114852871+owdx1@users.noreply.github.com>
2023-07-01 14:25:26 +0300

    Create viralAdvertising.cpp

---
+++ viralAdvertising.cpp
@@ -0,0 +1,116 @@
+/*
+HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly  people on social media.
+
+On the first day, half of those  people (i.e., ) like the advertisement and each shares it with  of their friends. At the beginning of the second day,  people receive the advertisement.
+
+Each day,  of the recipients like the advertisement and will share it with  friends on the following day. Assuming nobody receives the advertisement twice, determine how many people have liked the ad by the end of a given day, beginning with launch day as day .
+
+Example
+.
+
+Day Shared Liked Cumulative
+1      5     2       2
+2      6     3       5
+3      9     4       9
+4     12     6      15
+5     18     9      24
+The progression is shown above. The cumulative number of likes on the  day is .
+
+Function Description
+
+Complete the viralAdvertising function in the editor below.
+
+viralAdvertising has the following parameter(s):
+
+int n: the day number to report
+Returns
+
+int: the cumulative likes at that day
+Input Format
+
+A single integer, , the day number.
+
+Constraints
+
+Sample Input
+
+3
+Sample Output
+
+9
+Explanation
+
+This example is depicted in the following diagram:
+
+strange ad.png
+
+ people liked the advertisement on the first day,  people liked the advertisement on the second day and  people liked the advertisement on the third day, so the answer is . */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'viralAdvertising' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+int viralAdvertising(int n) {
+    int day = 1;
+    int shared = 5;
+    int liked = 0;
+    int cumulative = 0;
+    while( day <= n){
+        liked = shared / 2;
+        shared = liked * 3;
+        cumulative += liked;
+        
+        day++;
+    }
+    return cumulative;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    int result = viralAdvertising(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
